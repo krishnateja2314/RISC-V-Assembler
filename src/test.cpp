1b756edeaf69ad288b3f99b9ef4f54d860c15d9b
harsh241082 <145460069+harsh241082@users.noreply.github.com>
2024-08-27 22:32:42 +0530

    run the test cases

--- src/test.cpp
+++ src/test.cpp
@@ -1,14 +1,14 @@
 #include "../include/parser.h"
 using namespace std;
 #include <iostream>
-
+#include<sstream>
 int main()
 {
 
     Instruction result;
-    string s = "L1: addi x9, x10, 0xa";
+    string s = "sd x5, 8(x3)";
     result = parseInstruction(s);
     cout << result.mnemonic << result.rd << result.rs1 << result.immediate << endl
          << result.instructionInfo.opcode << result.instructionInfo.funct3 << result.instructionInfo.funct7;
-    return 0;
+     return 0;
 }
